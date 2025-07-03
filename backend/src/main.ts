feff369c291e64efe31802b6c9cf0dffa44a812e
Oğuz Berkay Bal <oguzberkaybal@icloud.com>
2025-07-04 01:41:52 +0300

    last fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -39,12 +39,9 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      'http://localhost:3001',
-      'http://localhost:3002',
-      'http://127.0.0.1:3000',
-      'http://127.0.0.1:3001',
-      'http://127.0.0.1:3002'
+      'https://portfolio-frontend-imdw.onrender.com',
     ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
